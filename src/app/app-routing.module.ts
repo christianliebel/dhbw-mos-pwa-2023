1505f49a72e1753c139a470cd0ef95d2f92c1467
Christian Liebel <christian.liebel@thinktecture.com>
2023-02-28 11:42:07 +0100

    Lab #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "about", component: AboutComponent },
+  { path: "todos", component: TodosComponent },
+  { path: "", pathMatch: "full", redirectTo: "todos" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
