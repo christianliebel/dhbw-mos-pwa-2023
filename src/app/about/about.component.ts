ab7f85aa0ad385147e51c8b7d95c1d7659c3e3ad
Christian Liebel <christian.liebel@thinktecture.com>
2023-03-01 11:19:17 +0100

    Lab #11

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor(private swPush: SwPush, private httpClient: HttpClient) {}
 
+  async register() {
+    // 1. Pushregistrierung anfragen
+    const pushSubscription = await this.swPush.requestSubscription({
+      serverPublicKey: 'BLI8zF79Z1kCQq72RgzYs0WtQ0ojY3XCqPwmgcNP-8LJIeXRep9sv6h41hErJDewrm3WDbFMPyyPhYO7-ClXabQ',
+    });
+
+    // 2. Pushregistrierung an Backend übertragen
+    await firstValueFrom(this.httpClient.post('http://localhost:3030/push', pushSubscription));
+  }
 }
